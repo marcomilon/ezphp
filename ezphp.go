2c6cfb5edbfae7d00f68dec9d61c69e83e7ae9eb
Marco Milon <marco.milon@gmail.com>
2018-03-26 16:12:48 -0500

    Update error handler when downloading php

--- ezphp.go
+++ ezphp.go
@@ -3,30 +3,41 @@ package main
 import (
 	"fmt"
 	"github.com/marcomilon/ezphp/installer"
+	"os"
 	"os/exec"
 )
 
 func main() {
 
-	fmt.Printf("[EzPhp] Launching to EzPHP\n")
-    fmt.Printf("[EzPhp] https://github.com/marcomilon/ezphp\n")
+	fmt.Println("[EzPhp] Launching to EzPHP")
+	fmt.Println("[About] https://github.com/marcomilon/ezphp")
 
 	path, err := searchPhpBin()
 	if err != nil {
-		fmt.Printf("[EzPhp] php not found\n")
-		path, err = installer.Install()
+		path, err = installer.PathToPhp()
+		if err != nil {
+			path, err = installer.Install()
+		}
 	}
 
 	if err != nil {
 		fmt.Printf("[Error] %s\n", err.Error())
-        return
+		return
 	}
+    
+    fmt.Printf("[EzPhp] Using php located in: %s\n", path)
+    servePhp(path)
 
-    command := exec.Command(path, "-S", "localhost:8888")
-    execErr := command.Run()
+}
+
+func servePhp(path string) {
+	command := exec.Command(path, "-S", "localhost:" + installer.Port, "-t", installer.DocumentRoot)
+	command.Stdout = os.Stdout
+	command.Stderr = os.Stderr
+	execErr := command.Run()
 	if execErr != nil {
-        fmt.Printf("[Error] Unable to execute PHP. %s\n", execErr.Error())
-        fmt.Printf("[Error] php path is. %s\n", path)
+		fmt.Printf("[Error] Unable to execute PHP: %s", execErr.Error())
+		fmt.Printf("[Error] php is located in: %s", path)
 	}
 }
 
