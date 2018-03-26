b86a1cac591adbd6ac1efad65d9986536938f035
Marco Milon <marco.milon@avantica.net>
2018-03-26 16:36:50 -0500

    Update readme and error handling

--- ezphp.go
+++ ezphp.go
@@ -36,8 +36,10 @@ func servePhp(path string) {
 	command.Stderr = os.Stderr
 	execErr := command.Run()
 	if execErr != nil {
-		fmt.Printf("[Error] Unable to execute PHP: %s", execErr.Error())
-		fmt.Printf("[Error] php is located in: %s", path)
+		fmt.Printf("[Error] Unable to execute PHP: %s\n", execErr.Error())
+		fmt.Printf("[Error] php is located in: %s\n", path)
+        fmt.Println("[Error] php require to have the Visual C++ Redistributable for Visual Studio 2017")
+        fmt.Println("[Error] Download Visual C++ from here: https://www.microsoft.com/en-us/download/details.aspx?id=48145")
 	}
 }
 
