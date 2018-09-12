47858947142e5d9b0ebd5017942ec2d4b9d334f0
Marco Milon <marco.milon@gmail.com>
2018-09-12 17:40:19 -0500

    Set all output's color to green

--- internals/output/output.go
+++ internals/output/output.go
@@ -2,30 +2,31 @@ package output
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/fatih/color"
 )
 
 func Info(s string) {
-	green := color.New(color.FgGreen, color.Bold)
-	green.Printf("[%-9s] ", "Info")
+	green := color.New(color.FgGreen)
+	green.Printf("| %s | ", time.Now().Format(time.Stamp))
 	fmt.Print(s)
 }
 
 func Error(s string) {
-	red := color.New(color.FgRed, color.Bold)
-	red.Printf("[%-9s] ", "Error")
+	red := color.New(color.FgGreen)
+	red.Printf("| %s | ", time.Now().Format(time.Stamp))
 	fmt.Print(s)
 }
 
 func Installer(s string) {
-	green := color.New(color.FgGreen, color.Bold)
-	green.Printf("[%-9s] ", "Installer")
+	green := color.New(color.FgGreen)
+	green.Printf("| %s | ", time.Now().Format(time.Stamp))
 	fmt.Print(s)
 }
 
 func Custom(level string, s string) {
-	green := color.New(color.FgGreen, color.Bold)
-	green.Printf("[%-9s] ", level)
+	green := color.New(color.FgGreen)
+	green.Printf("[%s] ",  time.Now().Format(time.Stamp))
 	fmt.Print(s)
 }
