ed3d3effc73af644c2833a5f58a8cab37956e381
Marco Milon <marco.milon@gmail.com>
2018-09-26 14:35:13 -0500

    Add a target parameter to WhereIsLocalPHP

--- internals/helpers/fs/fs.go
+++ internals/helpers/fs/fs.go
@@ -6,19 +6,17 @@ import (
 	"path/filepath"
 )
 
-const local = "phpbin"
-
 func WhereIsGlobalPHP(phpExe string) (string, error) {
 	return exec.LookPath(phpExe)
 }
 
-func WhereIsLocalPHP(phpExe string) (string, error) {
+func WhereIsLocalPHP(phpExe string, target string) (string, error) {
 
 	var err error
 
-	if _, err = os.Stat(local); err == nil {
-		absPath, _ := filepath.Abs(filepath.Dir(local))
-		defaultExecPath := absPath + string(os.PathSeparator) + local + string(os.PathSeparator) + phpExe
+	if _, err = os.Stat(target); err == nil {
+		absPath, _ := filepath.Abs(filepath.Dir(target))
+		defaultExecPath := absPath + string(os.PathSeparator) + target + string(os.PathSeparator) + phpExe
 		return defaultExecPath, nil
 	}
 
