dd7a377080f3a210aaa42d3758ba4fac15e77c78
Marco Milon <marco.milon@gmail.com>
2019-01-15 18:40:36 -0500

    Add ui IO

--- gui/ui.go
+++ gui/ui.go
@@ -1,20 +1,63 @@
 package gui
 
 import (
+	"fmt"
+
 	"github.com/andlabs/ui"
 )
 
-func StartUI() {
+type UiIO struct {
+	TextArea *ui.MultilineEntry
+}
+
+func (uiIo UiIO) Write(b []byte) (int, error) {
+	s := string(b[0:])
+	fmt.Print(s)
+
+	return len(b), nil
+}
+
+func (uiIo UiIO) Info(s string) {
+	ui.QueueMain(func() {
+		uiIo.TextArea.Append(s)
+	})
+}
+
+func (uiIo UiIO) Error(s string) {
+	ui.QueueMain(func() {
+		uiIo.TextArea.Append(s)
+	})
+}
+
+func (uiIo UiIO) Custom(tag string, s string) {
+	fmt.Print(s)
+}
+
+func (uiIo UiIO) Confirm(question string) bool {
+
+	// var confirmation string
+	//
+	// io.Info(fmt.Sprintf("%s [y/N]? ", question))
+	// fmt.Scanln(&confirmation)
+	//
+	// confirmation = strings.TrimSpace(confirmation)
+	// confirmation = strings.ToLower(confirmation)
+	//
+	// if confirmation == "y" {
+	// 	return true
+	// }
+
+	return false
+}
+
+func StartUI() (*ui.Window, *ui.MultilineEntry) {
 	err := ui.Main(func() {
-		window := ui.NewWindow("EzPHP", 800, 600, false)
-		window.SetMargined(true)
 
+		window := ui.NewWindow("EzPHP", 800, 600, true)
 		vbox := ui.NewVerticalBox()
-		vbox.SetPadded(true)
-
 		label := ui.NewMultilineEntry()
-        
-        vbox.Append(label, true)
+
+		vbox.Append(label, true)
 
 		window.SetChild(vbox)
 		window.OnClosing(func(*ui.Window) bool {
@@ -23,11 +66,11 @@ func StartUI() {
 		})
 
 		window.Show()
-		//go counter(label)
+		
+		
 	})
-	if err != nil {
-		panic(err)
-	}
+
+	return label
 }
 
 // func counter(label *ui.Label) {
